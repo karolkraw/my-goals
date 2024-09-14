aa4c4b4ce6bb3b7e134b62fcf10b5bbd30f99a45
Karol <karolkrawczyk8650@gmail.com>
2024-09-14 12:21:18 +0200

    create models

--- my_goals/models.py
+++ my_goals/models.py
@@ -1,10 +1,22 @@
 from django.db import models
+from django.utils import timezone
 
-class Task(models.Model):
-    title = models.CharField(max_length=200)  # Task title
-    description = models.TextField(blank=True)  # Optional description
-    completed = models.BooleanField(default=False)  # Whether the task is completed or not
-    created_at = models.DateTimeField(auto_now_add=True)  # Timestamp when the task was created
+class Goal(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    created_date = models.DateField(default=timezone.now)  # Set default to current date
+    deadline = models.DateField(default=timezone.now)
 
     def __str__(self):
-        return self.title 
+        return self.title
+
+class SubTask(models.Model):
+    goal = models.ForeignKey(Goal, related_name='subtasks', on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    completed = models.BooleanField(default=False)
+    created_date = models.DateField(default=timezone.now)  # Set default to current date
+    deadline = models.DateField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
+
