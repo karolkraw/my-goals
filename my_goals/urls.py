364a3e02fbd3cf7f43028ae4f132ee56fdc2f2b8
Karol <karolkrawczyk8650@gmail.com>
2024-09-15 14:45:22 +0200

    add sectionName to urls

--- my_goals/urls.py
+++ my_goals/urls.py
@@ -3,11 +3,13 @@ from . import views
 
 urlpatterns = [
     # ---------- Goals URLs ----------
-    path('goals/', views.goal_list, name='goal-list'),
-    path('goals/create/', views.goal_create, name='goal-create'),
-    path('goals/<int:pk>/', views.goal_detail, name='goal-detail'),
-    path('goals/<int:pk>/update/', views.goal_update, name='goal-update'),
-    path('goals/<int:pk>/delete/', views.goal_delete, name='goal-delete'),
+    path('goals/<str:sectionName>/', views.goal_list, name='goal-list'),
+    path('goals/create/<str:sectionName>/', views.goal_create, name='goal-create'),
+    path('goals/<str:sectionName><int:pk>/', views.goal_detail, name='goal-detail'),
+    path('goals/update/<str:sectionName>/<int:pk>/', views.goal_update, name='goal-update'),
+    path('goals/delete/<str:sectionName>/<int:pk>/', views.goal_delete, name='goal-delete'),
+    path('goals/complete/<str:sectionName>/<int:goal_id>/', views.complete_goal, name='complete-goal'),
+
 
     # ---------- SubTasks URLs ----------
     path('subtasks/', views.subtask_list, name='subtask-list'),
