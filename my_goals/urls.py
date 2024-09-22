0d17941afa3eaebc1b46e8d649c505bd374f378b
Karol <karolkrawczyk8650@gmail.com>
2024-09-22 21:28:42 +0200

    add poll url

--- my_goals/urls.py
+++ my_goals/urls.py
@@ -4,10 +4,12 @@ from . import views
 urlpatterns = [
     # ---------- Goals URLs ----------
     path('goals/<str:sectionName>/', views.goal_list, name='goal-list'),
+    path('goals/history/<str:sectionName>/', views.goal_history_list, name='history-list'),
     path('goals/create/<str:sectionName>/', views.goal_create, name='goal-create'),
     path('goals/update/<str:sectionName>/<int:pk>/', views.goal_update, name='goal-update'),
     path('goals/delete/<str:sectionName>/<int:pk>/', views.goal_delete, name='goal-delete'),
-    path('goals/complete/<str:sectionName>/<int:goal_id>/', views.complete_goal, name='complete-goal'),
+    path('goals/complete/<str:sectionName>/<str:goalTitle>/', views.complete_goal, name='complete-goal'),
+    path('goals/poll_history/<str:sectionName>/', views.poll_goal_history, name='poll-goal-history'),
 
 
     # ---------- SubTasks URLs ----------
@@ -16,5 +18,4 @@ urlpatterns = [
     path('goals/subtasks/<int:pk>/update/', views.subtask_update, name='subtask-update'),
     path('goals/subtasks/delete/<str:sectionName>/<str:goalTitle>/<str:subtaskTitle>/', views.subtask_delete, name='subtask-delete'),
     path('goals/subtasks/complete/<str:sectionName>/<str:goalTitle>/<str:subtaskTitle>/', views.subtask_complete, name='subtask-complete'),
-
 ]
